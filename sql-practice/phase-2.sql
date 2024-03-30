b05a76d23fa0341e010de014bc6485abf5eb913e
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-03-30 19:00:45 +0100

    finished phase 1 and 2
    More tables = probably bad design
    purchases are usually one to many
    timestamp default values
    CHECK CAST AND EVEN TRIGGERS all could be valuable resources for a good software design

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,20 @@
 -- Your code here
+DROP TABLE IF EXISTS customers,coffee_orders;
+
+CREATE TABLE customers {
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    phone_number TEXT CHECK (
+                            phone_number LIKE '%[0-9]%' AND
+                            NOT phone_number LIKE '%[^0-9]%' AND
+                            LENGTH(phone_number) <= 10),
+    email VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+};
+
+CREATE TABLE coffee_orders {
+    id INTEGER PRIMARY KEY,
+    is_redeemed BOOLEAN DEFAULT 0,
+    ordered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    customer_id INTEGER REFERENCES customers(id),
+};
