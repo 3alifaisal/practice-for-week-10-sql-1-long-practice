5ebb7dca739ef7804fc9ea1096bada754a62bd3d
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-03-31 13:57:00 +0200

    finished phase 3
    implemented a trigger to handle points automatically and inserted data into the table
    possible more improvement : trigger for deleting an order to reupdate the points automatically as well

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,20 +1,38 @@
 -- Your code here
-DROP TABLE IF EXISTS customers,coffee_orders;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS coffee_orders;
 
-CREATE TABLE customers {
+CREATE TABLE customers (
     id INTEGER PRIMARY KEY,
     name VARCHAR(40) NOT NULL,
-    phone_number TEXT CHECK (
-                            phone_number LIKE '%[0-9]%' AND
-                            NOT phone_number LIKE '%[^0-9]%' AND
-                            LENGTH(phone_number) <= 10),
+    phone_number TEXT,
+    points INTEGER DEFAULT 10,
     email VARCHAR(255) UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-};
+);
 
-CREATE TABLE coffee_orders {
+CREATE TABLE coffee_orders (
     id INTEGER PRIMARY KEY,
     is_redeemed BOOLEAN DEFAULT 0,
     ordered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    customer_id INTEGER REFERENCES customers(id),
-};
+    customer_id INTEGER REFERENCES customers(id)
+);
+
+CREATE TRIGGER PreventRedeemedOrderInsert
+BEFORE INSERT ON coffee_orders
+BEGIN
+    SELECT RAISE(ABORT, 'Not enough points') 
+    WHERE NEW.is_redeemed = 1 AND (SELECT points FROM customers WHERE id = NEW.customer_id) < 5;
+END;
+
+CREATE TRIGGER UpdatePointsAfterOrder
+AFTER INSERT ON coffee_orders
+BEGIN
+    UPDATE customers
+    SET points = 
+        CASE 
+            WHEN NEW.is_redeemed = 1 THEN points - 5
+            ELSE points + 1
+        END
+    WHERE id = NEW.customer_id;
+END;
